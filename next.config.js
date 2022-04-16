dfd258dfa4608cda8a9f8900b2aff0b1015aaa5c
Toshi Nakano <gtoshinakano@gmail.com>
2022-04-16 21:30:58 +0900

    Changed loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
-const withNextOptimizedImages = require('next-optimized-images');
 
-module.exports = withNextOptimizedImages({
+module.exports = {
   assetPrefix: isProd ? '/uiux-profile/' : '',
-})
+}
