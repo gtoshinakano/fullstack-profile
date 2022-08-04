47a0224473b01b0b20fe47cf2bdf4abd581fe00a
Toshi Nakano <gtoshinakano@gmail.com>
2022-08-04 15:51:38 +0900

    changed i18n config

--- i18n.config.js
+++ i18n.config.js
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "ja", "pt-BR"]
+    locales: ["en", "ja", "pt-BR"],
+    localePath: isProd ? path.resolve('./public/locales') : "./public/locales"
   }
 }
