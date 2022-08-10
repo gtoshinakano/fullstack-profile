e33d3e77909443a13e900e6e47335586dcdd4b45
Toshi Nakano <gtoshinakano@gmail.com>
2022-08-10 22:56:51 +0900

    Add prefix on router push

--- src/components/dom/ChangeLanguage/index.tsx
+++ src/components/dom/ChangeLanguage/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router"
+import prefix from "@/helpers/prefix"
 
 const ChangeLanguage = () => {
   const router = useRouter()
@@ -6,13 +7,13 @@ const ChangeLanguage = () => {
   return (
     <div className="fixed z-[1000] top-1 right-1 pr-4 text-white font-wask flex gap-3 ">
       <h5 className="font-thin">Change Language</h5>
-      <button onClick={() => router.push("/en")}>
+      <button onClick={() => router.push(prefix + "/en")}>
         English
       </button>
-      <button onClick={() => router.push("/ja")}>
+      <button onClick={() => router.push(prefix + "/ja")}>
         Japanese
       </button>
-      <button onClick={() => router.push("/pt-BR")}>
+      <button onClick={() => router.push(prefix + "/pt-BR")}>
         Portuguese
       </button>
     </div>
