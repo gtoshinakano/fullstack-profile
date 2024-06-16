6199701944ce1ee5c35906e111004d2a9d2672c0
Gabriel Toshinori Nakano <gtoshinakano@gmail.com>
2024-06-16 20:55:34 +0900

    next export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  output: 'export',
   assetPrefix: isProd ? '/fullstack-profile/' : '',
   images: {
     loader: 'custom',
