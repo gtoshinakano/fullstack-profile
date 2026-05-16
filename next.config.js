5e8314a3ad6747f4d34b658102e7da77243b373f
Gabriel Nakano <gtoshinakano@gmail.com>
2026-05-17 05:04:55 +0900

    feat: added pipeline for dev

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const isProd = process.env.NODE_ENV === 'production'
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/fullstack-profile'
 
 module.exports = {
   output: 'export',
-  assetPrefix: isProd ? '/fullstack-profile/' : '',
+  assetPrefix: isProd ? `${basePath}/` : '',
   images: {
     loader: 'custom',
   },
